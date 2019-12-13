0db97cfbae2079d862cd6229a95043d699943d4a
geysafran <geysafrancaromao@gmail.com>
2019-12-13 19:05:38 -0200

    alterei footer e menu

--- src/main/Footer.js
+++ src/main/Footer.js
@@ -6,19 +6,64 @@ import Retangulo2 from '../img/Rectangle2.png'
 export default function Rodape(){
     return(
         <Footer>
-            <figure>
-                <img src={Retangulo2} alt="Rodapé"/>
-            </figure>
+            <div className="container-footer">
+                <div className="footer-menu">
+                    <a href="#" className="item-fter">Página inicial</a>
+                    <a href="#" className="item-fter">Sobre nós</a>
+                    <a href="#" className="item-fter">Serviços</a>
+                    <a href="#" className="item-fter">Contato</a>
+                </div>
+                <div className="footer-login">
+                    <a href="#" className="item-fter">Fazer login</a>
+                    <a href="#" className="item-fter">Fazer inscrição</a>
+                </div>
+
+            </div>
         </Footer>
     );
 }
 
 const Footer = styled.footer`
-    figure{
-        padding: 0em;
-        margin: 0em;
-    }
-    img{
-        width: 100%;
-    }
+   .container-footer{
+    width: 100%;
+    height: 34vh;
+    background-image: linear-gradient(to top,#4f76e8,white);
+   justify-content: space-between;
+    display: flex;
+    align-items: flex-end;
+
+
+   }
+
+   .item-fter a:hover {
+       text-decoration: none;
+       color: white;
+   }
+
+   .footer.menu {
+       display: flex;
+       flex-direction: column;
+       
+
+   }
+   .footer-login{
+
+
+
+   }
+
+   .item-fter{
+       text-decoration: none;
+       color: white;
+       font-size: 1rem;
+       margin: 2vh;
+       font-size: 1em;
+       display: flex;
+   }
+       
+
+
+
+   }
+
 `;
--- src/main/Top.js
+++ src/main/Top.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import retangulo from '../img/Rectangle.png';
+
 import logo from '../img/logo1.svg';
 import login from '../img/login.svg';
 
@@ -11,13 +11,26 @@ export default function Top(){
             <div className="container-menu">
             <img className="logo" src={logo} alt="Logo" />
                 <nav className="menu">
-                        <a href="#" className="item-menu">Home</a>
-                        <a href="#" className="item-menu">About</a>
-                        <a href="#" className="item-menu">Services</a>
-                        <a href="#" className="item-menu">Contact</a>
+                        <a href="#" className="item-menulist">Página inicial</a>
+                        <a href="#" className="item-menulist">Sobre nós</a>
+                        <a href="#" className="item-menulist">Serviços</a>
+                        <a href="#" className="item-menulist">Contato</a>
                     </nav>
+                    <div className="box-login">
+                        <img src={login} 
+                        alt="login" 
+                        className="button-login"
+                        /> Entrar
+
+                        <button 
+                        className="button-sigin"
+                        >Inscrever-se
+                        </button>
+                        
+                        {/* <p className=""></p> */}
+                    </div>
                 
-                        <img className="login" src={login} alt="login" />
+                       
                    
             
                 </div>
@@ -28,8 +41,9 @@ export default function Top(){
 const Header = styled.header`
     .container-menu{
        display: flex;
+       padding: 3vw;
        margin-bottom: 10vh;
-      justify-content: center;
+        justify-content: center;
        height: 29vh;
        background-image: linear-gradient(to top, white , #3e6fff);
     }
@@ -44,17 +58,43 @@ const Header = styled.header`
         
     }
     .menu {
-        width: 61%;
-        font-size: 1.6em;
+        
+        width: 100%;
         text-align: center;
-        color: blue;
-        font-weight: 700;
+        
     }
-    a {
+
+    .box-login {
+        margin: 9vh;
+        width: 20%;
+    }
+    .item-menulist a {
+        color: black;
+        margin: 2vw;
+        font-size: 1.5em;
         text-decoration: none;
-        margin: 3vw;
-        color: blue
-        font-weigth: 700;
+      
+        
+    }
+
+    a:hover {
+        color: white;
+        text-decoration: none;
+    }
+
+    .button-login {
+
+    }
+
+    .button-sigin{
+        background-color: blue;
+        width: 12vw;
+        height: 6vh;
+        border: none;
+        border-radius: 9px;
+        color: white;
+}
+
     }
 
 
